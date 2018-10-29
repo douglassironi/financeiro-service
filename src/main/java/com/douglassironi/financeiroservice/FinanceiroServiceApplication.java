5a2c4763d9892f92396ea632ceea3da343400266
Douglas Sironi <ds@mbp-dslocal.local>
2018-10-29 19:46:10 -0300

    setup project

---
+++ src/main/java/com/douglassironi/financeiroservice/FinanceiroServiceApplication.java
@@ -0,0 +1,12 @@
+package com.douglassironi.financeiroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceiroServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceiroServiceApplication.class, args);
+	}
+}
